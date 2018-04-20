5e971a8c2a0615ed6533c26ec12884829eeb234c
Paco Espinel <francisco.espinel@road-track.com>
2018-04-19 20:26:04 -0500

    The PicoyPlaca software that said if you can drive in a specific Date and Time according to your PlateNumber

---
+++ PyP/PicoyPlaca/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace PicoyPlaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbxPlaca = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Verify = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.nHour = new System.Windows.Forms.NumericUpDown();
+            this.nMinutes = new System.Windows.Forms.NumericUpDown();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxPlaca
+            // 
+            this.tbxPlaca.Location = new System.Drawing.Point(91, 37);
+            this.tbxPlaca.Name = "tbxPlaca";
+            this.tbxPlaca.Size = new System.Drawing.Size(100, 20);
+            this.tbxPlaca.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.tbxPlaca, "XXX0000");
+            this.tbxPlaca.TextChanged += new System.EventHandler(this.tbxPlaca_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Placa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tiempo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fecha";
+            // 
+            // Verify
+            // 
+            this.Verify.Location = new System.Drawing.Point(33, 164);
+            this.Verify.Name = "Verify";
+            this.Verify.Size = new System.Drawing.Size(94, 29);
+            this.Verify.TabIndex = 6;
+            this.Verify.Text = "Verify";
+            this.Verify.UseVisualStyleBackColor = true;
+            this.Verify.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(91, 127);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(100, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // nHour
+            // 
+            this.nHour.Location = new System.Drawing.Point(91, 84);
+            this.nHour.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.nHour.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nHour.Name = "nHour";
+            this.nHour.Size = new System.Drawing.Size(36, 20);
+            this.nHour.TabIndex = 8;
+            this.nHour.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nMinutes
+            // 
+            this.nMinutes.Location = new System.Drawing.Point(133, 84);
+            this.nMinutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.nMinutes.Name = "nMinutes";
+            this.nMinutes.Size = new System.Drawing.Size(36, 20);
+            this.nMinutes.TabIndex = 9;
+            this.nMinutes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 242);
+            this.Controls.Add(this.nMinutes);
+            this.Controls.Add(this.nHour);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Verify);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxPlaca);
+            this.Name = "Form1";
+            this.Text = "PicoyPlaca";
+            ((System.ComponentModel.ISupportInitialize)(this.nHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxPlaca;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Verify;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.NumericUpDown nHour;
+        private System.Windows.Forms.NumericUpDown nMinutes;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ PyP/PicoyPlaca/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Globalization;
+
+namespace PicoyPlaca
+{
+    public partial class Form1 : Form
+    {
+        public string InpTime, InputDate;
+        public int LastNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsPlateNumberCorrect( string PlateN)
+        {
+                
+            bool ret = false;
+            //Verify the PlateNumber has the format AAA000
+            var PlateFormat = new Regex("^[a-zA-Z0-9]*$");
+            var StringsFormat = new Regex("^[a-zA-Z]*$");
+            var NumbersFormat= new Regex("^[0-9]*$");
+            string PlateNumbers = PlateN.Substring(3, (PlateN.Length - 3));
+            string PlateLetters = PlateN.Substring(0, 3);
+
+            if (PlateFormat.IsMatch(PlateN) &&
+                !StringsFormat.IsMatch(PlateN.Substring(3, 1)) &&
+                NumbersFormat.IsMatch(PlateNumbers) &&
+                PlateLetters.Length == 3 &&
+                PlateNumbers.Length == 4)
+            {
+                LastNumber = int.Parse(PlateNumbers.Substring(3, 1));
+                ret = true;
+            }
+            else
+            {
+                MessageBox.Show("Enter the plate number without spaces or special letters \nExample: AAA0000", "Invalid Plate Number");
+            }
+
+            return ret;
+
+        }
+
+        static bool QueryPyP( int QLastNum, string QTime, int PHour, int PMinutes)
+        {
+            bool IsInPyP = false; 
+            ArrayList PypRules = new ArrayList()
+            {
+                new PyPData {
+                    Days = "Lunes",
+                    LNumbers = new int[2] {1,2},
+                },
+
+                new PyPData {
+                    Days = "Martes",
+                    LNumbers = new int[2] {3,4},
+                },
+
+                new PyPData {
+                    Days = "Miercoles",
+                    LNumbers = new int[2] {5,6},
+                },
+                new PyPData {
+                    Days = "Jueves",
+                    LNumbers = new int[2] {7,8},
+                },
+                new PyPData {
+                    Days = "Viernes",
+                    LNumbers = new int[2] {9,10},
+                },
+
+            };
+
+            var pypEnum = PypRules.OfType<PyPData>();
+
+            var InNumber = from PyPData in pypEnum
+                           where ( PyPData.LNumbers.Contains(QLastNum)  &&
+                                   ( (PHour <= PyPData.EndHour[0] && PHour >= PyPData.InitHour[0]) || 
+                                     (PHour <= PyPData.EndHour[1] && PHour >= PyPData.InitHour[1]) ) &&
+                                    PyPData.Days.Equals(QTime, StringComparison.InvariantCultureIgnoreCase) 
+                                  )
+                           select PyPData;
+
+            //Search in the database the Number and compare with the date and time
+            foreach (var PyPData in InNumber)
+            {
+                IsInPyP = true;
+                //Control output
+                //Console.WriteLine("{0} Day {1}XXX",PyPData.Days, PyPData.LNumbers);
+            }
+
+            return IsInPyP;
+
+        }
+
+        private void tbxPlaca_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxPlaca.Text.Length == 0)
+            {
+                tbxPlaca.Text = "Ex: AAA0000";
+                tbxPlaca.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!tbxPlaca.Text.Equals("") )
+                {
+                    string PlateNumber = tbxPlaca.Text;
+                    string Date = dateTimePicker1.Value.ToString("dddd", new CultureInfo("es-ES"));
+                    int Hour = (int)nHour.Value;
+                    int Minutes = (int)nMinutes.Value;
+                    if (IsPlateNumberCorrect(PlateNumber))
+                    {
+
+                        if (QueryPyP(LastNumber, Date, Hour, Minutes))
+                        {
+                           MessageBox.Show("You may not drive the car is in PYP");
+                        }
+                        else
+                        {
+                           MessageBox.Show("The car is not in PYP");
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Datos incorrectos y/o campos vacios.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+       
+        }
+    }
+
+    class PyPData
+    {
+        public string Days { get; set; }
+        public int[] InitHour { get; set; }
+        public int[] InitMinutes { get; set; }
+        public int[] EndHour { get; set; }
+        public int[] EndMinutes { get; set; }
+        public int[] LNumbers { get; set; }
+
+
+        public PyPData ()
+        {
+            Days = "NoDay";
+            InitHour = new int[2] { 7, 16 };
+            InitMinutes = new int[2] { 0, 0 };
+            EndHour = new int[2] { 9, 19 };
+            EndMinutes = new int[2] { 30, 30 };
+            LNumbers = new int[2] { 0, 0 };
+            
+        }
+        
+    }
+
+}
